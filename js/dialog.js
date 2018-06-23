761518ac877e0fa8af54b24b696da91627355648
Senneer <senneerr@gmail.com>
2018-06-23 11:24:50 +0700

    Добавил модуль перетаскивания окна настроек

---
+++ js/dialog.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function () {
+  var setupWindow = document.querySelector('.setup');
+  var dialogHandler = setupWindow.querySelector('.upload');
+
+  dialogHandler.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+
+    var startCoords = {
+      x: e.clientX,
+      y: e.clientY
+    };
+    var dragged = false;
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupWindow.style.top = (setupWindow.offsetTop - shift.y) + 'px';
+      setupWindow.style.left = (setupWindow.offsetLeft - shift.x) + 'px';
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+
+      if (dragged) {
+        var clickPreventDefaultHandler = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandler.removeEventListener('click', clickPreventDefaultHandler);
+        };
+        dialogHandler.addEventListener('click', clickPreventDefaultHandler);
+      }
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  });
+})();
